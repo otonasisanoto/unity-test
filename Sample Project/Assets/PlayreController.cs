e29c1d2eeabcd90d7cf81506367c524384a64eb6
otonasisanoto <otonasisanoto@gamil.com>
2020-05-31 17:56:27 +0900

    Add Joystick

---
+++ Sample Project/Assets/PlayreController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayreController : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    public float speed;
+
+    void Update()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        float x = joystick.Horizontal * speed;
+        float y = joystick.Vertical * speed;
+        // 現在のpotionに値を加算してオグジェクトを移動
+        this.gameObject.transform.position = new Vector3(pos.x + x, pos.y + y, pos.z);
+    }
+}
